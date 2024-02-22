a4ada17e892d174eb722b851a90d0ad43d79f18e
kernzerfall <25184224+kernzerfall@users.noreply.github.com>
2024-02-23 00:11:04 +0100

    add init program

---
+++ src/args.rs
@@ -0,0 +1,25 @@
+use clap::{arg, Parser};
+use log::LevelFilter;
+
+#[derive(Parser, Debug, Clone)]
+pub(crate) struct CliArgs {
+    /// Tapo Username (E-Mail)
+    #[arg(short = 'u', long = "user")]
+    pub user: String, 
+    
+    /// Hub IP address
+    #[arg(short = 'i', long = "hub-ip")]
+    pub ip: String,
+
+    /// Device Identifier (if not unique)
+    #[arg(short = 's', long = "device", default_value = None)]
+    pub device: Option<String>,
+
+    /// Update interval (in seconds)
+    #[arg(short = 'n', long = "update-interval", default_value_t = 120)]
+    pub interval: u64,
+
+    /// Log level
+    #[arg(short = 'l', long = "log-level", default_value_t = LevelFilter::Info)]
+    pub log_level: LevelFilter,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,125 @@
-fn main() {
-    println!("Hello, world!");
+mod args;
+
+const KEYRING_SERVICE_NAME: &str = "home-temperature-statusbar";
+
+extern crate pretty_env_logger;
+
+#[macro_use]
+extern crate log;
+
+use std::time::Duration;
+
+use anyhow::anyhow;
+use clap::*;
+use tapo::responses::ChildDeviceResult::*;
+use tapo::responses::T31XResult;
+use tapo::ApiClient;
+
+use keyring::Entry;
+
+use args::CliArgs;
+use inquire::*;
+use tapo::HubHandler;
+use tokio::main;
+use tokio::time::sleep;
+
+fn set_creds(user: &str, pass: &str) -> Result<(), anyhow::Error> {
+    let entry = Entry::new(KEYRING_SERVICE_NAME, user)?;
+    entry.set_password(pass)?;
+    info!("Keyring entry for {user} was set");
+    Ok(())
+}
+
+fn ask_creds() -> Result<String, InquireError> {
+    inquire::Password::new("Tapo password > ")
+        .with_custom_confirmation_message("Confirm       > ")
+        .with_display_mode(PasswordDisplayMode::Masked)
+        .with_help_message(
+            "Enter the password of your Tapo account. \
+             It will be saved to your keyring.",
+        )
+        .prompt()
+}
+
+fn get_pass(user: &str) -> Result<String, anyhow::Error> {
+    let entry = Entry::new(KEYRING_SERVICE_NAME, user)?;
+
+    match entry.get_password() {
+        Err(keyring::Error::NoEntry) => {
+            let pass = ask_creds()?;
+            set_creds(user, &pass)?;
+            Ok(pass)
+        }
+        Err(keyring::Error::Ambiguous(_)) => {
+            error!("{KEYRING_SERVICE_NAME}, {user} is ambiguous in your keyring");
+            Err(anyhow!("K::AMBIG"))
+        }
+        Ok(res) => Ok(res),
+        _ => Err(anyhow!("K::UNKN")),
+    }
+}
+
+async fn setup(user: &str, pass: &str, ip: &str) -> Result<HubHandler, tapo::Error> {
+    ApiClient::new(user, pass)?.h100(ip).await
+}
+
+async fn get_print_temp_data(hub: &HubHandler) -> Result<(), anyhow::Error> {
+    let children = hub.get_child_device_list().await?;
+    debug!("Children data\n{children:#?}");
+
+    let temperature_data = children
+        .iter()
+        .filter_map(|c| match c {
+            T315(di) | T310(di) => Some(di),
+            _ => None,
+        })
+        .collect::<Vec<&Box<T31XResult>>>();
+
+    if temperature_data.is_empty() {
+        error!("No temperature sensors found");
+        println!("NO_SENS");
+        return Ok(());
+    }
+
+    if temperature_data.len() == 1 {
+        info!("Unambiguous data; only 1 sensor found; using it.");
+        let tdata = temperature_data.first().unwrap();
+        println!(
+            "\u{f07d0} {:02.1}\u{f0504} {:02}\u{e373}",
+            tdata.current_temperature, tdata.current_humidity
+        );
+    }
+    Ok(())
+}
+
+#[main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = CliArgs::parse();
+
+    pretty_env_logger::formatted_timed_builder()
+        .filter_level(args.log_level)
+        .init();
+
+    info!("hello");
+
+    // let hub = ApiClient::new("***REMOVED***", "***REMOVED***")?
+    //     .h100(args.ip.to_owned())
+    //     .await?;
+
+    let pass = get_pass(args.user.as_str())?;
+    let hub = setup(args.user.as_str(), pass.as_str(), args.ip.as_str()).await?;
+
+    let device_info = hub.get_device_info().await?;
+    debug!("Device Info\n{device_info:#?}");
+    info!(
+        "Connected to {} named {}",
+        device_info.r#type, device_info.nickname
+    );
+
+    loop {
+        info!("fetching");
+        get_print_temp_data(&hub).await?;
+        info!("sleeping");
+        sleep(Duration::from_secs(args.interval)).await;
+    }
 }
